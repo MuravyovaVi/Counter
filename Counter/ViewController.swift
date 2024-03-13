0923606e62174685a0034db137fe42075338f5ef
Vika Muravyova <muravyova.vi@ya.ru>
2024-03-13 18:09:51 +0300

    Приложение-счётчик создано

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countUILabel: UILabel!
+    
+   
+    @IBOutlet weak var plusUIButton: UIButton!
+    
+    
+    @IBOutlet weak var minusUIButton: UIButton!
+    
+    
+    @IBOutlet weak var refreshUIButton: UIButton!
+    
+    
+    @IBOutlet weak var historyUITextView: UITextView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        historyUITextView.isEditable = false
+        historyUITextView.isScrollEnabled = true
+        addToHistory("История изменений:")
+        updateCount()
     }
 
-
+    private var count = 0
+    
+    private func updateCount() {
+        countUILabel.text = "Значение счетчика: \(count)"
+    }
+    
+    private func addToHistory(_ text: String) {
+        historyUITextView.text.append("\(text)\n")
+    }
+   
+    @IBAction func plusButtonTapped(_ sender: UIButton) {
+        
+        count += 1
+        updateCount()
+        addToHistory("\(Date()): значение изменено на +1")
+    }
+    
+    @IBAction func minusButtonTapped(_ sender: UIButton) {
+        if count > 0 {
+            count -= 1
+            updateCount()
+            addToHistory("\(Date()): значение изменено на -1")
+        } else {
+            addToHistory("\(Date()): попытка уменьшить значение счётчика ниже 0")
+        }
+    }
+    
+    
+    @IBAction func refreshButtonTapped(_ sender: UIButton) {
+        count = 0
+        updateCount()
+        addToHistory("\(Date()): значение сброшено")
+    }
 }
 
